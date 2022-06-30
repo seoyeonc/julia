3bce891a80fa3544dc33cfbe9d98ca14f572403b
seoyeonc <chltjdus1212@gmail.com>
2022-06-30 11:49:34 +0000

    Deploying to gh-pages from  @ fc798095e62b6ca6d0788b917bca1bd6d42f5e7d 🚀

---
+++ 2022-06-30-st-4월21일.jl
@@ -0,0 +1,54 @@
+### A Pluto.jl notebook ###
+# v0.18.4
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ bebc88a8-c117-11ec-14da-bb8177016829
+md"""
+# 4월 21일
+"""
+
+# ╔═╡ 5977d558-5ce8-447e-8c28-558056dfb71c
+html"""
+<div style="display: flex; justify-content: center;">
+<div  notthestyle="position: relative; right: 0; top: 0; z-index: 300;">
+<iframe src=
+"
+https://www.youtube.com/embed/playlist?list=PLQqh36zP38-xaCXIzqLhRnDBPItsyZrzB
+"
+width=600 height=375  frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+"""
+
+# ╔═╡ 3b4cf20c-6d1f-43d5-b59e-87545afed98d
+md"""
+## 숙제
+"""
+
+# ╔═╡ 652523da-5c5c-4517-8d03-60e0649d8021
+md"""
+`-` 오늘은 숙제없습니다. 시험잘보세요.
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─bebc88a8-c117-11ec-14da-bb8177016829
+# ╟─5977d558-5ce8-447e-8c28-558056dfb71c
+# ╟─3b4cf20c-6d1f-43d5-b59e-87545afed98d
+# ╟─652523da-5c5c-4517-8d03-60e0649d8021
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
